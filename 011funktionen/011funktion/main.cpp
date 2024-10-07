647f96fc8f429f9cf1812a38ea3f21dfc4a739ee
BerndDonner <119803636+BerndDonner@users.noreply.github.com>
2024-10-07 15:12:52 +0200

    function overloading

--- 011funktionen/011funktion/main.cpp
+++ 011funktionen/011funktion/main.cpp
@@ -3,20 +3,32 @@
 
 using namespace std;
 
-double linear(double x, double m, double t)
-{
-    double y = m*x + t;
-    return y;
-//    return m*x + t;
-}
+double linear(double, double, double );
+int linear(int x, int m, int t);
 
 int main()
 {
     double x = -1.0;
+    double steigung = 3.0;
+    double y_achsenabschnitt = 100.0;
     double r1 = exp(x);
-    double r2 = linear(x, 3.0, 100.0);
+    double r2 = linear(x, steigung, y_achsenabschnitt);
+    int r3 = linear(3, 4, 5);
 
-    cout << "     e(" << x << ") = " << r1 << endl;
-    cout << "linear(" << x << ") = " << r2 << endl;
+    cout << "                e(" << x << ") = " << r1 << endl;
+    cout << "x = " << x << " ---> " << steigung << "*x + " << y_achsenabschnitt <<" = " << r2 << endl;
     return 0;
+}
+
+double linear(double x, double m, double t)
+{
+    double y = m*x + t;
+    return y;
+    //    return m*x + t;
+}
+
+int linear(int x, int m, int t)
+{
+    int y = m*x + t;
+    return y;
 }
