8232178e7fb003bf4107296c83a4bfc516c65340
BerndDonner <119803636+BerndDonner@users.noreply.github.com>
2024-10-14 13:03:26 +0200

    Update main.cpp

--- 011funktionen/main.cpp
+++ 011funktionen/main.cpp
@@ -6,22 +6,27 @@ using namespace std;
 double linear(double, double, double );
 int linear(int x, int m, int t);
 
-int main()
+void funktionstest1()
 {
     double x = -1.0;
     double steigung = 3.0;
     double y_achsenabschnitt = 100.0;
+
+    int x_int = 3;
+    int m_int = 4;
+    int t_int = 5;
     double r1 = exp(x);
     double r2 = linear(x, steigung, y_achsenabschnitt);
-    int r3 = linear(3, 4, 5);
+    int r3 = linear(x_int, m_int, t_int);
 
     cout << "                e(" << x << ") = " << r1 << endl;
     cout << "x = " << x << " ---> " << steigung << "*x + " << y_achsenabschnitt <<" = " << r2 << endl;
-    return 0;
+    cout << "x = " << x_int << " ---> " << m_int << "*x + " << t_int <<" = " << r3 << endl;
 }
 
 double linear(double x, double m, double t)
 {
+    cout << "Double Version wird verwendet\n";
     double y = m*x + t;
     return y;
     //    return m*x + t;
@@ -29,6 +34,23 @@ double linear(double x, double m, double t)
 
 int linear(int x, int m, int t)
 {
+    cout << "Int Version wird verwendet\n";
     int y = m*x + t;
     return y;
+}
+
+int main()
+{
+//    funktionstest1();
+    cout << "Sein oder nicht sein\ndas ist hier die Frage\nDas ist ein Backslash: \\\n";
+    cout << "Das sind zwei Backslashes: \\\\\n";
+    cout << "Dieter Nuhr: \"Wer keine Ahnung hat sollte einfach mal die Fresse halten.\" \n";
+    cout << "Das ist eine Übung\0 zu Escape Sequenzen.\n";
+    cout << "\nWas bist Du fuer ein Zeichen: \x5e\n";
+    char x = 'a';
+    char8_t z[] = u8"\u00ef";
+    char8_t y[] = u8"ß";
+
+    cout << y << "\n";
+    return 0;
 }
