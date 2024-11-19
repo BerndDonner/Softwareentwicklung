2263aecb73d28ce015e47fbe68bc5c2ea44b5fc1
Bernd Donner <bernd.donner@sabel.com>
2024-11-19 08:25:19 +0100

    zur Unterrichtvorbereitung

---
+++ flake.nix
@@ -0,0 +1,35 @@
+# run with:                nix develop
+# see metadata with:       nix flake metadata
+# debug with:              nix repl
+#                          :lf .#
+# check with:              nix flake check
+# If you want to update a locked input to the latest version, you need to ask
+# for it:                  nix flake lock --update-input nixpkgs
+# show available packages: nix-env -qa
+#                          nix search nixpkgs
+# show nixos version:      nixos-version
+# 
+
+{
+  description = "C++ Development with Nix 24.05";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc14Stdenv
+            cmake
+            gdb
+          ];
+          buildInputs = with pkgs; [];
+        };
+      };
+    };
+
+}
